52e850286a905809de57a10a68cf730bd92f85e4
Elie Bamunoba <78250384+ElieBamunoba@users.noreply.github.com>
2024-05-28 11:46:53 +0200

    Update main.tf

--- terraform-aws-ec2/main.tf
+++ terraform-aws-ec2/main.tf
@@ -12,10 +12,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "elie-dg-terraform-test" {
+resource "aws_instance" "terraform-test" {
   ami           = "ami-0f58b397bc5c1f2e8" 
   tags = {
-    Name = "elie-dg-terraform-test"
+    Name = "elie-dg-ansible-control-tf"
   }
   instance_type = "t2.micro"
   key_name      = "elie"
@@ -25,37 +25,7 @@ resource "aws_instance" "elie-dg-terraform-test" {
   # Security group rules
   security_groups = ["sg-06b382513307ae528"]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    # Update the system packages
-    apt update && apt upgrade -y
-    
-    # Install the necessary packages
-    apt install software-properties-common unzip -y
-    
-    # Add Ansible PPA and install Ansible
-    apt-add-repository --yes --update ppa:ansible/ansible
-    apt install ansible -y
-    
-    # Install the AWS CLI version 2
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install
-    
-    # Configure SSH key using the AWS Secrets Manager
-    mkdir -p /root/.ssh
-    
-    # Download and copy elie.pem from S3 to /root/.ssh/id_rsa
-    sudo aws s3 cp s3://dg-cohort-01/elie-key/elie.pem /root/.ssh/id_rsa
-    
-    chmod 600 /root/.ssh/id_rsa
-    
-    # Clone Git repository to /opt/dg-ansible-playbooks
-    GIT_SSH_COMMAND='ssh -i /root/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' git clone git@github.com:deployguru-learning/elie-bamunoba-dg-ansible.git /opt/dg-ansible-playbooks
-    
-    chown -R ubuntu:ubuntu /opt/dg-ansible-playbooks
-    chown -R ubuntu:ubuntu /root/.ssh
-  EOF
+  user_data = file("./userdata.sh")
 
   iam_instance_profile = "dg-ec2-base"
 }
